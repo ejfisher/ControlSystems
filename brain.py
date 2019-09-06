77eef770eebc417d5b4b51bd5358286695490756
Edward James Fisher <efisher@rams.colostate.edu>
2019-09-06 03:44:24 -0600

    modified brain.py to run a sodomized version of radio module code

--- brain.py
+++ brain.py
@@ -1,1 +1,98 @@
-#This files objective is to take data from sensor header files and output data in a standard format for other subsystems. 
+#This files objective is to take data from sensor header files and output data in a standard format for other subsystems.
+
+
+# Import Python System Libraries
+import time
+# Import Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# Import the SSD1306 module.
+import adafruit_ssd1306
+# Import the RFM69 radio module.
+import adafruit_rfm69
+# Import James' Garbage file
+import MPU 
+# Button A
+btnA = DigitalInOut(board.D5)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D6)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Button C
+btnC = DigitalInOut(board.D12)
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# 128x32 OLED Display
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
+# Clear the display.
+display.fill(0)
+display.show()
+width = display.width
+height = display.height
+
+# Configure Packet Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
+prev_packet = None
+# Optionally set an encryption key (16 byte AES key). MUST match both
+# on the transmitter and receiver (or be set to None to disable/the default).
+rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
+
+while True:
+    packet = None
+    # draw a box to clear the image
+    display.fill(0)
+    display.text('RasPi Radio', 35, 0, 1)
+
+    # check for packet rx
+    packet = rfm69.receive()
+    if packet is None:
+        display.show()
+        display.text('- Waiting for PKT -', 15, 20, 1)
+    else:
+        # Display the packet text and rssi
+        display.fill(0)
+        prev_packet = packet
+        packet_text = str(prev_packet, "utf-8")
+        display.text('RX: ', 0, 0, 1)
+        display.text(packet_text, 25, 0, 1)
+        time.sleep(1)
+
+    if not btnA.value:
+        # Send Button A
+        display.fill(0)
+        MPU.gyro()
+        button_a_data = bytes("Button A!\r\n","utf-8")
+        rfm69.send(button_a_data)
+        display.text("Gyroscope", 35, 0, 1)
+        #display.text(str(tuple), 18, 15, 1)
+    elif not btnB.value:
+        # Send Button B
+        display.fill(0)
+        MPU.acc()
+        button_b_data = bytes("Button B!\r\n","utf-8")
+        rfm69.send(button_b_data)
+        display.text("Accelerometre", 35, 0, 1)
+        #display.text(str(tuple), 18, 15, 1)
+    elif not btnC.value:
+        # Send Button C
+        display.fill(0)
+        MPU.rotation()
+        button_c_data = bytes("Button C!\r\n","utf-8")
+        rfm69.send(button_c_data)
+        display.text("Rotation", 35, 0, 1)
+        #display.text(str(tuple), 18, 15, 1)
+
+    display.show()
+    time.sleep(0.1)
